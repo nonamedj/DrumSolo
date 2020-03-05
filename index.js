6f5dd459296a655dfcdb66130b30a5c3372e24b2
Szabi <kriskoszabi@gmail.com>
2020-03-05 01:53:55 +0100

    Drum Kit

---
+++ index.js
@@ -0,0 +1,59 @@
+var numberDrumButtons = document.getElementsByClassName("drum").length;
+for (var i = 0; i < numberDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTTML = this.innerHTML;
+    makeSound(buttonInnerHTTML);
+    makeAnimation(buttonInnerHTTML);
+  });
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    makeAnimation(event.key);
+});
+
+function makeSound(key) {
+
+  switch(key) {
+    case "w":
+      var audio1 = new Audio('sounds/tom-1.mp3');
+      audio1.play();
+      break;
+    case "a":
+      var audio2 = new Audio('sounds/tom-2.mp3');
+      audio2.play();
+      break;
+    case "s":
+      var audio3 = new Audio('sounds/tom-3.mp3');
+      audio3.play();
+      break;
+    case "d":
+      var audio4 = new Audio('sounds/tom-4.mp3');
+      audio4.play();
+      break;
+    case "j":
+      var audio5 = new Audio('sounds/snare.mp3');
+      audio5.play();
+      break;
+    case "k":
+      var audio6 = new Audio('sounds/crash.mp3');
+      audio6.play();
+      break;
+    case "l":
+      var audio7 = new Audio('sounds/kick-bass.mp3');
+      audio7.play();
+      break;
+
+    default:
+  }
+}
+
+function makeAnimation(currentKey) {
+
+  var activeButton= document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){ activeButton.classList.remove("pressed"); }, 200);
+}
